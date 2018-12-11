43acc5c1d2e92bd4a3a8b81770731012fe4dcff1
Bryan <38082187+MeyerBryan@users.noreply.github.com>
2018-12-11 15:47:13 -0800

    TPath android

---
+++ TPath2/app/src/main/java/com/example/chrono/tpath/Aid.kt
@@ -0,0 +1,12 @@
+package com.example.chrono.tpath
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Aid : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aid)
+    }
+}
