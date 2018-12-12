55ee3ec40141019eb2489485fb9b75dce97c1f74
Bryan <38082187+MeyerBryan@users.noreply.github.com>
2018-12-12 01:19:17 -0800

    update under grad and added post

--- TPath2/app/src/main/java/com/example/chrono/tpath/Postgrad1.kt
+++ TPath2/app/src/main/java/com/example/chrono/tpath/Postgrad1.kt
@@ -1,12 +1,24 @@
 package com.example.chrono.tpath
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_checklist.*
+import kotlinx.android.synthetic.main.activity_postgrad1.*
 
 class Postgrad1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_postgrad1)
+        Aidpageback.setOnClickListener{
+            val intent = Intent(this, Aid::class.java)
+            startActivity(intent)
+        }
+
+        Under.setOnClickListener {
+            val intent = Intent(this, Checklist::class.java)
+            startActivity(intent)
+        }
     }
 }
